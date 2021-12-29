32175f965d08e43dcb63381017a7e75ab448b9df
Chris-Pagan <christopher.pagan@outlook.com>
2021-12-29 03:52:55 -0800

    all tests pass in qualified

---
+++ src/mergeSort.js
@@ -0,0 +1,35 @@
+function mergeSort(compare, elements) {
+  if (Array.isArray(elements)){
+    if (elements.length <= 1){
+      return elements;
+    }
+    const middle = Math.floor(elements.length /2);
+    const leftElements = elements.slice(0, middle);
+    const rightElements = elements.slice(middle);
+
+    const leftSorted = mergeSort(compare, leftElements)
+    const rightSorted = mergeSort(compare, rightElements);
+    return merge(compare, leftSorted, rightSorted);
+  }
+  return elements;
+}
+
+function merge(compare, left, right){
+  const sorted = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length){
+    const comparison = compare(left[leftIndex], right[rightIndex]);
+    if (comparison < 0){
+      sorted.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      sorted.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return sorted.concat(leftIndex < left.length ? left.slice(leftIndex) : right.slice(rightIndex))
+}
+
+module.exports = mergeSort;
